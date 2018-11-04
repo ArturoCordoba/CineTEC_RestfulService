5e7384256f733e550a4efeffe17db0f6b32b1119
Arturo Córdoba <arturocv16@gmail.com>
2018-11-04 13:39:39 -0600

    Add project files.

---
+++ WebRole1/Models/Actor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRole1.Models
+{
+    public class Actor
+    {
+        public int Id_actor { get; set; }
+
+        public string F_name { get; set; }
+
+        public string FL_name { get; set; }
+
+        public string SL_name { get; set; }
+    }
+}
---
+++ WebRole1/Models/Connection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRole1.Models
+{
+    public class Connection
+    {
+        //Localhost connection string
+        //public static string connectionString = "Server=localhost; Port=5433; User Id=postgres; Password=covicoar; Database=CINETEC;";
+
+        //Azure server connection string
+        public static string connectionString = "Server=cinetecdb.postgres.database.azure.com;Database=CINETEC;Port=5432;User Id=cinetec_admin@cinetecdb;Password=cine123tec$;SslMode=Require;";
+    }
+}
